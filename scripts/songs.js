399054d9263c30b5677786de7df73713376444e5
João Armênio <joaoarmeniosilveira@gmail.com>
2023-07-10 14:54:40 -0300

    ajeitando as aparencias ainda e fzd o player

---
+++ scripts/songs.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        src: "../assets/audios/Are_You_Impressed.mp3",
+        name: "Are You Impressed",
+        author: "Honey Revenge",
+    },
+    {
+        src: "../assets/audios/Call_Me_Maybe.mp3",
+        name: "Call Me Maybe",
+        author: "Carly Rae Jepsen",
+    },
+    {
+        src: "../assets/audios/Childhood_Dreams.mp3",
+        name: "Childhood Dreams",
+        author: "Seraphine, Jasmine Clarke",
+    },
+    {
+        src: "../assets/audios/Dandelions.mp3",
+        name: "Dandelions",
+        author: "Ruth B.",
+    }
+]
