e2c92a85667dd702b85d143d26ceaed99fe62723
Bakuretsy <joaoarmeniosilveira@gmail.com>
2023-07-31 21:33:56 -0300

    terminar resizer.js

---
+++ scripts/resizer.js
@@ -0,0 +1,40 @@
+const resizer = document.querySelector('#resizer')
+const sidebar = document.querySelector('#sidebar')
+
+function initResizerFn( resizer, sidebar ) 
+{
+   let mousePos, w;
+
+   function rs_mousedownHandler( e ) 
+   {
+        mousePos = e.clientX;
+
+        let sbWidth = window.getComputedStyle( sidebar ).width;
+        w = parseInt( sbWidth, 10 );
+
+        document.addEventListener("mousemove", rs_mousemoveHandler);
+        document.addEventListener("mouseup", rs_mouseupHandler);
+   }
+
+   function rs_mousemoveHandler( e ) 
+   {
+        let dx = e.clientX - mousePos;
+
+        let cw = w + dx; // complete width
+        
+        if ( cw <= 1000 ) {
+            sidebar.style.width = `${ cw }px`;
+        }
+   }
+
+   function rs_mouseupHandler() {
+      // remove event mousemove && mouseup
+      document.removeEventListener("mouseup", rs_mouseupHandler);
+      document.removeEventListener("mousemove", rs_mousemoveHandler);
+   }
+
+   resizer.addEventListener("mousedown", rs_mousedownHandler);
+}
+
+
+initResizerFn( resizer, sidebar );
