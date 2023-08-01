c19647da4b8bcf0ce4d3cb630fe91d43a3db3c10
JOAO ARMENIO SILVEIRA <jas21@inf.ufpr.br>
2023-08-01 17:01:59 -0300

    aaa

--- scripts/resizer.js
+++ scripts/resizer.js
@@ -1,39 +1,39 @@
 const resizer = document.querySelector('#resizer')
 const sidebar = document.querySelector('#sidebar')
+const mainContent = document.querySelector('#main-content')
 
 function initResizerFn( resizer, sidebar ) 
 {
-   let mousePos, w;
+   let mousePos, w
 
    function rs_mousedownHandler( e ) 
    {
-        mousePos = e.clientX;
+      mousePos = e.clientX
 
-        let sbWidth = window.getComputedStyle( sidebar ).width;
-        w = parseInt( sbWidth, 10 );
+      let sbWidth = window.getComputedStyle( sidebar ).width
+      w = parseInt( sbWidth, 10 )
 
-        document.addEventListener("mousemove", rs_mousemoveHandler);
-        document.addEventListener("mouseup", rs_mouseupHandler);
+      document.addEventListener("mousemove", rs_mousemoveHandler)
+      document.addEventListener("mouseup", rs_mouseupHandler)
    }
 
    function rs_mousemoveHandler( e ) 
    {
-        let dx = e.clientX - mousePos;
+      let dx = e.clientX - mousePos
+      let cw = w + dx; // complete width
 
-        let cw = w + dx; // complete width
-        
-        if ( cw <= 800 && cw >= 250 )  {
-            sidebar.style.width = `${ cw }px`;
-        }
+      if ( cw <= 800 && cw >= 250 ) {
+         sidebar.style.width = `${ cw }px`
+      }
    }
 
    function rs_mouseupHandler() {
       // remove event mousemove && mouseup
-      document.removeEventListener("mouseup", rs_mouseupHandler);
-      document.removeEventListener("mousemove", rs_mousemoveHandler);
+      document.removeEventListener("mouseup", rs_mouseupHandler)
+      document.removeEventListener("mousemove", rs_mousemoveHandler)
    }
 
-   resizer.addEventListener("mousedown", rs_mousedownHandler);
+   resizer.addEventListener("mousedown", rs_mousedownHandler)
 }
 
 
