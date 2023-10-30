959b0631d97d0de91a0a15e7e4cc7a135b220138
JOAO ARMENIO SILVEIRA <jas21@inf.ufpr.br>
2023-10-30 20:45:22 -0300

    [ADD] music-bars in Sidebar

--- scripts/songList.js
+++ scripts/songList.js
@@ -6,7 +6,7 @@ const sidebarSongsCover = document.querySelector(".sidebar-songs-cover")
 const createSongCard = () => {
     for (const song of songs) {
         const cardDiv = document.createElement("div")
-        cardDiv.classList.add('sb-card', 'd-flex', 'align-items-center', 'rounded-2', 'p-2', 'text-nowrap', 'overflow-hidden')
+        cardDiv.classList.add('sb-card', 'd-flex', 'align-items-center', 'rounded-2', 'p-2', 'text-nowrap', 'overflow-hidden', 'justify-content-between')
 
         const cardImg = document.createElement("img")
         cardImg.src = song.songCover
@@ -14,7 +14,7 @@ const createSongCard = () => {
         cardImg.style.height = '3rem'
 
         const cardInfo = document.createElement("div")
-        cardInfo.classList.add('d-flex', 'flex-column', 'align-items-start', 'justify-content-between')
+        cardInfo.classList.add('d-flex', 'flex-row', 'align-items-start', 'justify-content-between')
         
         const songNameSpan = document.createElement("span")
         songNameSpan.classList.add('sb-song-name', 'pb-1')
@@ -24,11 +24,27 @@ const createSongCard = () => {
         songAuthorSpan.classList.add('sb-song-author')
         songAuthorSpan.textContent = song.author
 
-        cardInfo.appendChild(songNameSpan)
-        cardInfo.appendChild(songAuthorSpan)
+        const songInfoDiv = document.createElement("div")
+        songInfoDiv.classList.add('d-flex', 'flex-column', 'align-items-start', 'justify-content-between')
 
-        cardDiv.appendChild(cardImg)
+        // add the animated music bars div
+        const musicBarsDiv = document.createElement("div")
+        musicBarsDiv.classList.add('music-bars')
+        
+        // add the 3 bars span
+        for (let i = 0; i < 3; i++) {
+            const barSpan = document.createElement("span")
+            musicBarsDiv.appendChild(barSpan)
+        }
+
+        songInfoDiv.appendChild(songNameSpan)
+        songInfoDiv.appendChild(songAuthorSpan)
+        cardInfo.appendChild(cardImg)
+        cardInfo.appendChild(songInfoDiv)
+
         cardDiv.appendChild(cardInfo)
+
+        cardDiv.appendChild(musicBarsDiv)
 
         cardDiv.addEventListener('click', () => {
             playSong(song)
